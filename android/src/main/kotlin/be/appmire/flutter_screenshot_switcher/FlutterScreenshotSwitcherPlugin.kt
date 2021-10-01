1b557fb7221f882bec24bf21727b3bb743000370
wferem1 <wouter.feremans@nexuzhealth.com>
2021-10-01 13:30:52 +0200

    add support for v2 embedding

--- android/src/main/kotlin/be/appmire/flutter_screenshot_switcher/FlutterScreenshotSwitcherPlugin.kt
+++ android/src/main/kotlin/be/appmire/flutter_screenshot_switcher/FlutterScreenshotSwitcherPlugin.kt
@@ -2,37 +2,63 @@ package be.appmire.flutter_screenshot_switcher
 
 import android.app.Activity
 import android.view.WindowManager
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
 
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterScreenshotSwitcherPlugin(private val context: Activity): MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      if (null == registrar.activity()) {
-         return;
-      }
-      val channel = MethodChannel(registrar.messenger(), "flutter_screenshot_switcher")
-      channel.setMethodCallHandler(FlutterScreenshotSwitcherPlugin(registrar.activity()))
-    }
+/** FlutterScreenshotSwitcherPlugin */
+class FlutterScreenshotSwitcherPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+  private lateinit var activity : FlutterActivity
+  private lateinit var binaryMessenger: BinaryMessenger
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    binaryMessenger = flutterPluginBinding.binaryMessenger
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity as FlutterActivity
+    channel = MethodChannel(binaryMessenger, "flutter_screenshot_switcher")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if( call.method.equals("disableScreenshots")) {
-      context.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+
+      activity.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE,
               WindowManager.LayoutParams.FLAG_SECURE)
       result.success(null)
     }
     else if( call.method.equals("enableScreenshots")) {
-      context.window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+      activity.window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
       result.success(null)
     }
     else {
       result.notImplemented()
     }
   }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {}
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
+
+  override fun onDetachedFromActivity() {}
+
 }
